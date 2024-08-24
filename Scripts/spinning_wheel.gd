586815ff9927622f4ad2483fd6f31765f65443db
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2024-08-24 18:25:05 +0530

    Update spinning whell

--- Scripts/spinning_wheel.gd
+++ Scripts/spinning_wheel.gd
@@ -5,86 +5,43 @@ extends Control
 @export var power: int = 2
 @export var reward_position = 0
 signal sig_reward
+
 var vat_pham = [
-	{
-		"name": "Dark blue",
-		"from": 0,
-		"to": 45,
-		"ma_vat_pham": 200,
-		"ten_vat_pham": "Thanh Long"
-	},
-	{
-		"name": "Dark green",
-		"from": 45,
-		"to": 90,
-		"ma_vat_pham": 0,
-		"ten_vat_pham": "Gạch"
-	},
-	{
-		"name": "Blue",
-		"from": 90,
-		"to": 135,
-		"ma_vat_pham": 204,
-		"ten_vat_pham": "Chanh"
-	},
-	{
-		"name": "Yellow",
-		"from": 135,
-		"to": 180,
-		"ma_vat_pham": 203,
-		"ten_vat_pham": "Dưa Hấu"
-	},
-	{
-		"name": "Purple",
-		"from": 180,
-		"to": 225,
-		"ma_vat_pham": 201,
-		"ten_vat_pham": "Sầu Riêng"
-	},
-	{
-		"name": "Green",
-		"from": 225,
-		"to": 270,
-		"ma_vat_pham": 0,
-		"ten_vat_pham": "Gạch"
-	},
-	{
-		"name": "Orange",
-		"from": 270,
-		"to": 315,
-		"ma_vat_pham": 202,
-		"ten_vat_pham": "Vãi"
-	},
-	{
-		"name": "Pink",
-		"from": 315,
-		"to": 360,
-		"ma_vat_pham": 0,
-		"ten_vat_pham": "Gạch"
-	}
-	]
+	{"name": "A", "from": 0, "to": 45, "ma_vat_pham": 200, "ten_vat_pham": "Thanh Long"},
+	{"name": "H", "from": 45, "to": 90, "ma_vat_pham": 0, "ten_vat_pham": "Gạch"},
+	{"name": "G", "from": 90, "to": 135, "ma_vat_pham": 204, "ten_vat_pham": "Chanh"},
+	{"name": "F", "from": 135, "to": 180, "ma_vat_pham": 203, "ten_vat_pham": "Dưa Hấu"},
+	{"name": "E", "from": 180, "to": 225, "ma_vat_pham": 201, "ten_vat_pham": "Sầu Riêng"},
+	{"name": "D", "from": 225, "to": 270, "ma_vat_pham": 0, "ten_vat_pham": "Gạch"},
+	{"name": "C", "from": 270, "to": 315, "ma_vat_pham": 202, "ten_vat_pham": "Vãi"},
+	{"name": "B", "from": 315, "to": 360, "ma_vat_pham": 0, "ten_vat_pham": "Gạch"}
+]
+
+var chosen_items = []  # A list to track items that have already been selected
 
 func _on_btn_spin_pressed():
-	if is_spin == false:
+	if not is_spin:
+		if chosen_items.size() == vat_pham.size():
+			print("All items have been selected! Resetting the wheel...")
+			chosen_items.clear()  # Reset chosen items if all have been selected
+
 		is_spin = true
 		var tween = get_tree().create_tween().set_parallel(true)
 		tween.connect("finished", func():
-			#after tween finish animation, this function is call
-			var old_rotation_degrees = %Wheel.rotation_degrees
-			#set is_spin = false to tell for user can press again
 			is_spin = false
-			if old_rotation_degrees > 360:
-				#This part is to fix the error that when rotating the steamer once, it will not rotate counterclockwise
-				var rad_ = fmod(old_rotation_degrees, 360)
-				%Wheel.rotation_degrees = rad_
-			)
-		reward_position = randi_range(0, 360) #random position from 0 to 360 degrees
+			%Wheel.rotation_degrees = fmod(%Wheel.rotation_degrees, 360)
+			check_reward_position()
+		)
+		reward_position = randi_range(0, 360)
+		tween.tween_property(%Wheel, "rotation_degrees", reward_position + 360 * speed * power, 3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
 
-		for item in vat_pham:
-			if reward_position >= item.from - 22.5 and reward_position <= item.to - 22.5:
-				print(item.name)
-				#signal for another scene
+func check_reward_position():
+	for item in vat_pham:
+		if reward_position >= item.from and reward_position < item.to:
+			if item.name in chosen_items:
+				print("Already selected this option. Spin again!")
+			else:
+				print("Chosen: " + item.name)
 				sig_reward.emit(item.ma_vat_pham)
-		tween.tween_property(%Wheel, "rotation_degrees", reward_position +  360 * speed * power , 3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
-		tween.finished
-	
+				chosen_items.append(item.name)  # Mark the item as chosen
+			break
